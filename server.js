c273ec8123b3cefbe618a29191ae5be080c165ef
Alvin Yearby <leroy.yearby@gmail.com>
2022-01-09 11:49:13 -0500

    updated server.js

--- server.js
+++ server.js
@@ -7,5 +7,78 @@ const connection = mysql.createConnection({
     port: '3306',
     user: 'root',
     password: 'Rr!528183', 
-    database:'', 
+    database:'employees', 
 }); 
+
+connection.connect(function(err) {
+    if (err) throw err 
+    console.log("connected as id" + connection.threadId)
+    questions(); 
+})
+
+function questions() {
+    inquier
+        .prompt({
+            name: 'options',
+            type: 'list',
+            message: 'What would you like to do?',
+            choices: [
+                'View all employees',
+                'View all departments',
+                'View all roles',
+                'Add an employee',
+                'Add a department', 
+                'Add a role',
+                'Change employee role',
+                'Delete an employee',
+                'None'
+            ]
+        }).then(function(answer) {
+            switch(answer.action){
+                case 'View all employees':
+                    viewEmp();
+                    break;
+                case 'View all departments':
+                    viewDep(); 
+                    break; 
+                case 'View all Roles':
+                    viewRoles();
+                    break;
+                case 'Add an employee':
+                    addEmp();
+                    break;
+                case 'Add a department':
+                    addDep();
+                    break;
+                case 'Add a role':
+                    addRole();
+                    break; 
+                case 'Change employee role':
+                    changeRole(); 
+                    break; 
+                case 'Delete an employee':
+                    deleteEmp();
+                    break;
+                case 'None': 
+                    leaveApp();
+                    break;
+                default:
+                    break; 
+            }
+        })
+
+}; 
+
+function viewEmp() {
+    var query = 'SELECT * FROM employee'; 
+    connection.query(query, function(err, res) {
+        if (err) throw err; 
+        console.log(res.length + 'Found employees!');
+        console.table('Employees', res);
+        questions(); 
+    })
+}; 
+
+function viewDep() {
+    
+}
